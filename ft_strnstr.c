8ad7ed906ecaecfe9cd2d3b14fc67dde11c34ceb
Midna321 <midnatwitch@gmail.com>
2024-08-10 23:45:06 +0200

    More functions added

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*to_find2;
+
+	i = 0;
+	j = 0;
+	if (to_find == NULL || to_find[0] == '\0')
+		return ((char *)str);
+	while (str[i] != '\0' && i < len)
+	{
+		if (str[i] == to_find[j])
+		{
+			while (str[i + j] == to_find[j] && i + j < len)
+			{
+				if (to_find[j + 1] == '\0')
+					return ((char *)str + i);
+				j++;
+			}
+			j = 0;
+		}
+		i++;
+	}
+	return (NULL);
+}
+/*#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf("%s\n",ft_strnstr("ciaone", "a", 2));
+}*/
