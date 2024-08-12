ec77259cfe326fb710cc321558842decaca1f0ad
Midna321 <midnatwitch@gmail.com>
2024-08-12 19:36:26 +0200

    almost norminette friendly split

--- ft_split.c
+++ ft_split.c
@@ -13,9 +13,9 @@ static int	word_count(const char *str)
 		i++;
 	while (str[i])
 	{
-		if ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13)
-			|| ((str[i + 1] == '\0') && ((str[i + 1] != ' ')
-					|| (str[i + 1] <= 9 && str[i + 1] >= 13))))
+		if ((str[i] != ' ' && (str[i] < 9 || str[i] > 13))
+			&& ((str[i + 1] == ' ') || (str[i + 1] >= 9 && str[i + 1] <= 13)
+				|| str[i + 1] == '\0'))
 			words++;
 		i++;
 	}
@@ -35,6 +35,19 @@ static char	*ft_strncpy(char *dest, char *src, int n)
 	return (orig);
 }
 
+char *create_word(char *start, size_t length, char c)
+{
+    char *word;
+
+    word = (char *)malloc(sizeof(char) * (length + 2)); // +2: uno per il carattere c e uno per il terminatore null
+    if (!word)
+        return (NULL);
+    ft_strncpy(word, start, length);
+    word[length] = c;
+    word[length + 1] = '\0';
+    return (word);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	size_t		word_len;
@@ -45,27 +58,35 @@ char	**ft_split(const char *s, char c)
 
 	i = 0;
 	start = s;
-	words = (char **)malloc((sizeof (char *) * (word_count(s)) + 1));
+	words = (char **)malloc((sizeof (char *) * (word_count(s) + 1)));
+	if (!words)
+		return (NULL);
 	while (i < word_count(s))
 	{
 		while (*start == ' ' || (*start >= 9 && *start <= 13))
 			start++;
 		end = start;
-		while (*end != ' ' && *end != '\t')
+		while (*end != ' ' && *end != '\t' && *end != '\n' && *end)
 			end++;
 		word_len = end - start;
-		words[i] = (char *)malloc(sizeof (char) * (word_len + 1));
-		ft_strncpy(words[i], (char *)start, word_len);
-		words[i][word_len] = c;
+		words[i] = create_word((char *)start, word_len, c);
 		start = end;
 		i++;
 	}
+	words[word_count(s)] = NULL;
 	return (words);
 }
-/*int main()
+
+int main()
 {
-	int i = 0;
-	char **str = ft_split("A BOM BA ZZA",'+');
-	while(str[i])
-		printf("%s", str[i++]);
-}*/
+    int i = 0;
+    char **str = ft_split("   test1 test2 test3   ", '+');
+    while (str[i])
+    {
+        printf("%s", str[i]);
+        free(str[i]);
+        i++;
+    }
+    free(str);
+    return 0;
+}
